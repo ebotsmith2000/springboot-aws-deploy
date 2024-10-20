2bee68300e0d018953d830e7a684322a66e75143
ebotsmith2000 <ebotsmith@gmail.com>
2024-10-20 14:50:09 -0400

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -16,4 +16,9 @@ public class TestController {
     public String getMessage() {
         return "Second message from AWS Ecs";
     }
+
+      @GetMapping("/update")
+    public String getUpdate() {
+        return "Third message from AWS Ecs";
+    }
 }
